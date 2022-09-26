97b7d47feb7b806392c5c8e59f5d6e515da70680
williamd5 <willduncannn@gmail.com>
2022-09-26 09:41:37 +0300

    fix dts

--- lib/dkim.d.ts
+++ lib/dkim.d.ts
@@ -3,12 +3,12 @@ import Signature from "dkim-signature";
 
 export { Key, Signature };
 
-export function getKey(domain: any, selector: any, callback: (error: Error, key: Key) => any): void;
-export function getKey(domain: any, callback: (error: Error, key: Key) => any): void;
+export function getKey(domain: any, selector: any, callback: (error: Error, key?: Key) => any): void;
+export function getKey(domain: any, callback: (error: Error, key?: Key) => any): void;
 
 export function processBody(message: string | Buffer, method?: string): string;
 
-export function processHeader(headers: string[], signHeaders: any[], method: string): string;
+export function processHeader(headers: string[], signHeaders: string[], method: string): string;
 export function processHeader(headers: string[], method: string): string;
 
 interface Signature {
@@ -47,6 +47,6 @@ export interface VerifyResult {
   key: Key;
 }
 
-export function verify(message: Buffer, callback: (error: Error, result: VerifyResult[]) => any): void;
+export function verify(message: Buffer, callback: (error: Error, result?: VerifyResult[]) => any): void;
 
-export function verifySignature(body: Buffer, headers: any[], callback: (error: Error, result: VerifyResult) => any): void;
+export function verifySignature(body: Buffer, headers: any[], callback: (error: Error, result?: VerifyResult) => any): void;
